8479e1466797e0fbda03c1e434524c95a7a8c9bf
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-09-11 10:17:06 +0300

    NaN olan değerler yerine ortalamanın yazılması

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 missing_values = pd.read_csv("missing_values.csv")
+print(missing_values)
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+age = missing_values.iloc[:, 1:4].values
+print(age)
+
+imputer = imputer.fit(age[:, 1:4])
+age[:, 1:4] = imputer.transform(age[:, 1:4])
+print(age)
